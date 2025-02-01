495bbaf9cf63ac97ac594022a44d75a6a4efc998
saminemir <samiinemir@gmail.com>
2025-02-01 18:56:17 +0100

    V 1.0.0

--- script.js
+++ script.js
@@ -10,13 +10,21 @@ async function checkweather(city){
   const response= await fetch(apiUrl + city + `&appid=${apiKey}`);
   var data = await response.json();
 
-  console.log(data);
-  document.querySelector(".city").innerHTML = data.name ;
-  document.querySelector(".tempurature").innerHTML= Math.round(data.main.temp) + "°c";
-  document.querySelector(".humidity").innerHTML = Math.round(data.main.humidity) + "%" ;
-  document.querySelector(".wind").innerHTML = Math.round(data.wind.speed) + " km/h";
+  if(response.status == 404){
+    document.querySelector(".error").style.display= "block";
+    document.querySelector(".weather").style.display="none";
+  }
+  else{
+    document.querySelector(".city").innerHTML = data.name ;
+    document.querySelector(".tempurature").innerHTML= Math.round(data.main.temp) + "°c";
+    document.querySelector(".humidity").innerHTML = Math.round(data.main.humidity) + "%" ;
+    document.querySelector(".wind").innerHTML = Math.round(data.wind.speed) + " km/h";
+  
+    weatherIcon.src = `images/${data.weather[0].main}.png`;
+    document.querySelector(".error").style.display= "none";
+    document.querySelector(".weather").style.display="block";
+  }
 
-  weatherIcon.src = `images/${data.weather[0].main}.png`;
 }
 
 searchBtn.addEventListener("click", ()=>{
